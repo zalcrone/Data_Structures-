bbb4187306db6a5e9ab0dcb3ea70bd85e9abe9c8
Aman Singh <68295480+zalcrone@users.noreply.github.com>
2020-10-04 23:33:10 +0530

    Update Binary_Search_Tree.cpp

--- Binary_Search_Tree.cpp
+++ Binary_Search_Tree.cpp
@@ -96,6 +96,59 @@ class BST
         PrintPreOrder(r->left);
         PrintPreOrder(r->right);
     }
+    void PrintInOrder(TreeNode *r)
+    {
+        if(r==NULL)
+        {
+            return;
+        }
+        PrintInOrder(r->left);
+        cout<<r->data<<" ";
+        PrintInOrder(r->right);
+    }
+    
+    void PrintPostOrder(TreeNode *r)
+    {
+        if(r==NULL)
+        {
+            return;
+        }
+        PrintPostOrder(r->left);
+        PrintPostOrder(r->right);
+        cout<<r->data<<endl;
+        
+    }
+    
+    
+    TreeNode* iterativeSearch(int val)
+    {
+        if(root==NULL)
+        {
+            return root;
+            
+        }
+        else
+        {
+            TreeNode* temp = root;
+            while(temp!=NULL)
+            {
+                if(val==temp->data)
+                {
+                    return temp;
+                }
+                else if(val<temp->data)
+                {
+                    temp = temp->left;
+                }
+                else
+                {
+                    temp = temp->right;
+                }
+            }
+            return NULL;
+        }
+        
+    }
     
     
     
@@ -112,6 +165,9 @@ int main()
      cout<<"What option do you want to perform?\nSelect the option. Enter 0 to exit"<<endl;
      cout<<"1.Inserting"<<endl;
      cout<<"2.Printing Pre Order"<<endl;
+     cout<<"3.Print In Order"<<endl;
+     cout<<"4.Print Post Order"<<endl;
+     cout<<"5.IterativeSearch"<<endl;
      
      
      cin>>option;
@@ -133,20 +189,41 @@ int main()
         case 2:
         cout<<"PreOrder representation is:";
         obj.PrintPreOrder(obj.root);
-        
-        
         cout<<endl;
+        break;
         
+        case 3:
+        cout<<"InOrder representation is:";
+        obj.PrintInOrder(obj.root);
+        cout<<endl;
+        break;
         
-        default:
-        cout<<"Enter Proper Option Number"<<endl;
+        case 4:
+        cout<<"PostOrder representation is:";
+        obj.PrintPostOrder(obj.root);
+        cout<<endl;
+        break;
         
-        
-        
-        
+        case 5:
+        cout<<"IterativeSearch operation called"<<endl;
+        cout<<"Enter Value of TREE NODE to SEARCH in BST"<<endl;
+        cin>>dat;
+        new_node = obj.iterativeSearch(dat);
+        if(new_node!=NULL)
+        {
+            cout<<"Value Found"<<endl;
+        }
+        else
+        {
+            cout<<"Value Not Found"<<endl;
+        }
+        break;
        
+       default:
+       cout<<"Enter Proper Option Number"<<endl;
+       break;
      }
-    }
+   }
     while(option!=0);
     
     return 0;
